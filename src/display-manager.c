5008f242a81d4658cc20fda78b825117a0c85838
dingjingmaster <dingjing@live.cn>
2019-10-18 17:29:56 +0800

    添加日志

--- src/display-manager.c
+++ src/display-manager.c
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <djctool/clib_syslog.h>
 
 #include "display-manager.h"
 #include "configuration.h"
@@ -46,12 +47,14 @@ G_DEFINE_TYPE_WITH_PRIVATE (DisplayManager, display_manager, G_TYPE_OBJECT)
 DisplayManager *
 display_manager_new (void)
 {
+    CT_SYSLOG(LOG_INFO, "");
     return g_object_new (DISPLAY_MANAGER_TYPE, NULL);
 }
 
 GList *
 display_manager_get_seats (DisplayManager *manager)
 {
+    CT_SYSLOG(LOG_INFO, "");
     DisplayManagerPrivate *priv = display_manager_get_instance_private (manager);
     return priv->seats;
 }
@@ -59,6 +62,7 @@ display_manager_get_seats (DisplayManager *manager)
 Seat *
 display_manager_get_seat (DisplayManager *manager, const gchar *name)
 {
+    CT_SYSLOG(LOG_INFO, "");
     DisplayManagerPrivate *priv = display_manager_get_instance_private (manager);
 
     for (GList *link = priv->seats; link; link = link->next)
@@ -75,6 +79,7 @@ display_manager_get_seat (DisplayManager *manager, const gchar *name)
 static void
 check_stopped (DisplayManager *manager)
 {
+    CT_SYSLOG(LOG_INFO, "");
     DisplayManagerPrivate *priv = display_manager_get_instance_private (manager);
 
     if (priv->stopping &&
@@ -82,7 +87,8 @@ check_stopped (DisplayManager *manager)
         g_list_length (priv->seats) == 0)
     {
         priv->stopped = TRUE;
-        g_debug ("Display manager stopped");
+        CT_SYSLOG(LOG_DEBUG, "Display manager stopped");
+        CT_SYSLOG(LOG_INFO, "signal emit STOPPED");
         g_signal_emit (manager, signals[STOPPED], 0);
     }
 }
@@ -90,13 +96,18 @@ check_stopped (DisplayManager *manager)
 static void
 seat_stopped_cb (Seat *seat, DisplayManager *manager)
 {
+    CT_SYSLOG(LOG_INFO, "");
     DisplayManagerPrivate *priv = display_manager_get_instance_private (manager);
 
     priv->seats = g_list_remove (priv->seats, seat);
+    CT_SYSLOG(LOG_INFO, "G_SIGNAL_MATCH_DATA -> manager");
     g_signal_handlers_disconnect_matched (seat, G_SIGNAL_MATCH_DATA, 0, 0, NULL, NULL, manager);
 
     if (!priv->stopping)
+    {
         g_signal_emit (manager, signals[SEAT_REMOVED], 0, seat);
+        CT_SYSLOG(LOG_INFO, "signal emit SEAT_REMOVED");
+    }
 
     g_object_unref (seat);
 
@@ -106,16 +117,22 @@ seat_stopped_cb (Seat *seat, DisplayManager *manager)
 gboolean
 display_manager_add_seat (DisplayManager *manager, Seat *seat)
 {
+    CT_SYSLOG(LOG_INFO, "");
     DisplayManagerPrivate *priv = display_manager_get_instance_private (manager);
 
     g_return_val_if_fail (!priv->stopping, FALSE);
 
     gboolean result = seat_start (SEAT (seat));
     if (!result)
+    {
+        CT_SYSLOG(LOG_ERR, "seat start failture");
         return FALSE;
+    }
 
     priv->seats = g_list_append (priv->seats, g_object_ref (seat));
+    CT_SYSLOG(LOG_INFO, "SEAT_SIGNAL_STOPPED -> seat_stopped_cb");
     g_signal_connect (seat, SEAT_SIGNAL_STOPPED, G_CALLBACK (seat_stopped_cb), manager);
+    CT_SYSLOG(LOG_INFO, "signal emit SEAT_ADDED");
     g_signal_emit (manager, signals[SEAT_ADDED], 0, seat);
 
     return TRUE;
@@ -124,12 +141,13 @@ display_manager_add_seat (DisplayManager *manager, Seat *seat)
 void
 display_manager_start (DisplayManager *manager)
 {
+    CT_SYSLOG(LOG_INFO, "");
     g_return_if_fail (manager != NULL);
 
     /* Disable Plymouth if no X servers are replacing it */
     if (plymouth_get_is_active ())
     {
-        g_debug ("Stopping Plymouth, no displays replace it");
+        CT_SYSLOG(LOG_DEBUG, "Stopping Plymouth, no displays replace it");
         plymouth_quit (FALSE);
     }
 }
@@ -137,14 +155,13 @@ display_manager_start (DisplayManager *manager)
 void
 display_manager_stop (DisplayManager *manager)
 {
+    CT_SYSLOG(LOG_INFO, "stopping display manager");
     DisplayManagerPrivate *priv = display_manager_get_instance_private (manager);
 
     g_return_if_fail (manager != NULL);
 
     if (priv->stopping)
         return;
-
-    g_debug ("Stopping display manager");
 
     priv->stopping = TRUE;
 
@@ -163,20 +180,25 @@ display_manager_stop (DisplayManager *manager)
 static void
 display_manager_init (DisplayManager *manager)
 {
+    CT_SYSLOG(LOG_INFO, "");
     /* Load the seat modules */
     seat_register_module ("local", SEAT_LOCAL_TYPE);
     seat_register_module ("xremote", SEAT_XREMOTE_TYPE);
+    CT_SYSLOG(LOG_DEBUG, "local SEAT_LOCAL_TYPE");
+    CT_SYSLOG(LOG_DEBUG, "xremote SEAT_XREMOTE_TYPE");
 }
 
 static void
 display_manager_finalize (GObject *object)
 {
+    CT_SYSLOG(LOG_INFO, "");
     DisplayManager *self = DISPLAY_MANAGER (object);
     DisplayManagerPrivate *priv = display_manager_get_instance_private (self);
 
     for (GList *link = priv->seats; link; link = link->next)
     {
         Seat *seat = link->data;
+        CT_SYSLOG(LOG_DEBUG, "disconnect G_SIGNAL_MATCH_DATA");
         g_signal_handlers_disconnect_matched (seat, G_SIGNAL_MATCH_DATA, 0, 0, NULL, NULL, self);
     }
     g_list_free_full (priv->seats, g_object_unref);
@@ -187,6 +209,7 @@ display_manager_finalize (GObject *object)
 static void
 display_manager_class_init (DisplayManagerClass *klass)
 {
+    CT_SYSLOG(LOG_INFO, "");
     GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
     object_class->finalize = display_manager_finalize;
